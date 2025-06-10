5d152e43290ba9bbceb6f575c34ea535269a7807
shadcy <shreyashwanjari740@gmail.com>
2025-06-10 23:47:28 +0530

    need revesion

--- CodeForces CF MAY 25/Codeforces Round 1021 (Div. 2) A(TLE)/A. Vadim's Collection/main.cpp
+++ CodeForces CF MAY 25/Codeforces Round 1021 (Div. 2) A(TLE)/A. Vadim's Collection/main.cpp
@@ -1,32 +1,48 @@
 #include <iostream>
-#include <algorithm>
 #include <string>
+#include <vector>
+#include <algorithm>
 using namespace std;
 
-bool isBeautiful(const string& s) {
-    for (int i = 0; i < 10; ++i) {
-        if (s[i] - '0' < 9 - i) {
-            return false;
+string solve_beautiful_phone(string s) {
+    // Convert string to vector of integers
+    vector<int> digits;
+    for (char c : s) {
+        digits.push_back(c - '0');
+    }
+    
+    // Sort digits to make it easier to find smallest valid digits
+    sort(digits.begin(), digits.end());
+    
+    string result = "";
+    vector<bool> used(10, false);  // Track which digits we've used
+    
+    // For each position (0-indexed), find smallest valid digit
+    for (int pos = 0; pos < 10; pos++) {
+        int min_required = 9 - pos;  // Minimum digit required at this position
+        
+        // Find the smallest unused digit that satisfies the constraint
+        for (int i = 0; i < 10; i++) {
+            if (!used[i] && digits[i] >= min_required) {
+                result += to_string(digits[i]);
+                used[i] = true;
+                break;
+            }
         }
     }
-    return true;
-}
-
-string smallestBeautiful(string s) {
-    sort(s.begin(), s.end()); // Start with the smallest lexicographic permutation
-    do {
-        if (isBeautiful(s)) return s;
-    } while (next_permutation(s.begin(), s.end()));
-    return s; // In theory, this should never be reached since input is guaranteed beautiful
+    
+    return result;
 }
 
 int main() {
     int t;
     cin >> t;
+    
     while (t--) {
         string s;
         cin >> s;
-        cout << smallestBeautiful(s) << '\n';
+        cout << solve_beautiful_phone(s) << endl;
     }
+    
     return 0;
 }
